ae65aed623376ea9bb955d14586d498ffd681831
Yk Snow <hyuki3110@gmail.com>
2020-02-24 08:22:01 +0900

    ABC069

---
+++ atcoder/ABC069/D.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+#define rep1(i, n) for (ll i = 1; i <= (n); ++i)
+#define rrep(i, n) for (ll i = (n - 1); i >= 0; --i)
+#define ALL(obj) (obj).begin(), (obj).end()
+#define pb push_back
+#define to_s to_string
+#define len(v) (int)v.size()
+#define UNIQUE(v) v.erase(unique(v.begin(), v.end()), v.end())
+#define print(x) cout << (x) << '\n'
+#define debug(x) cout << #x << ": " << (x) << '\n'
+using namespace std;
+using ll = long long;
+typedef pair<ll, ll> P;
+ll MOD = 1e9 + 7;
+ll devc(ll x, ll y) { return 1 + (x - 1) / y; }
+
+int main()
+{
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    cout << fixed << setprecision(20);
+
+    ll H, W, N;
+    cin >> H >> W >> N;
+    vector<ll> A(N);
+    rep(i, N) cin >> A[i];
+
+    vector<ll> C;
+    rep(i, N) rep(j, A[i]) C.pb(i + 1);
+
+    rep(i, H){
+        if(i % 2 == 0) for (ll j = 0; j < W; ++j) cout << C[i*W+j] << " ";
+        else for (ll j = W - 1; j >= 0; --j) cout << C[i*W+j] << " ";
+        print("");
+    }
+
+    return 0;
+}
